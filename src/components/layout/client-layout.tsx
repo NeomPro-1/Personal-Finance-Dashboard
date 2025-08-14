83862a844bc60329e4f0aca892dc5dd115cf3ef2
neom music <neom.music27@gmail.com>
2025-08-14 14:04:59 +0000

    in mobile view, the navbar buttons are touching to the top and bottom, c

--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -19,7 +19,7 @@ export function ClientLayout({
             <SidebarContent />
           </Sidebar>
           <div className="flex-1 flex flex-col h-screen overflow-y-auto">
-            <header className="sticky top-0 z-10 flex h-14 items-center justify-between gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 md:hidden">
+            <header className="sticky top-0 z-10 flex h-14 items-center justify-between gap-4 border-b bg-background px-4 py-2 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 md:hidden">
               <SidebarTrigger />
               <ThemeToggle />
             </header>
