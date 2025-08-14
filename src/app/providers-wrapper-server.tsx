cd245ec36a0a234d4fbefa853da56dcded0eecf5
neom music <neom.music27@gmail.com>
2025-08-14 20:20:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/providers-wrapper-server.tsx
+++ src/app/providers-wrapper-server.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProvidersWrapperClient from './providers-wrapper-client'; // Import the client-only wrapper
+import ProvidersWrapperClient from './providers-wrapper-client';
 
 export default function ProvidersWrapperServer({ children }: { children: React.ReactNode }) {
   return <ProvidersWrapperClient>{children}</ProvidersWrapperClient>;
